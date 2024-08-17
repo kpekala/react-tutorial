4733ad50bbe4f2f113c10f600026e25c87160e42
kpekala <kgpekala@gmail.com>
2024-08-17 09:57:08 +0200

    add state to form

--- 05-far-away/src/App.js
+++ 05-far-away/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: false },
@@ -19,20 +21,39 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [count, setCount] = useState(1);
+
   function handleSubmit(e) {
+    if (!description) return;
     e.preventDefault();
+
+    const newItem = {
+      description,
+      count,
+      packed: false,
+      id: Date.now(),
+    };
+
+    setDescription('');
+    setCount(1);
   }
   return (
     <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={count} onChange={(e) => setCount(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item...' />
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
