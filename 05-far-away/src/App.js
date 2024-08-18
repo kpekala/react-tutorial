56024f3d3df833460803a5ea4af37ecbf2bc45fa
Konrad Pękala <kgpekala@gmail.com>
2024-08-18 20:06:07 +0200

    stats

--- 05-far-away/src/App.js
+++ 05-far-away/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -113,10 +113,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className='stats'>
+        <em>Start adding some items to your packing list 👍</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className='stats'>
-      <em>You have X items on your list and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? 'You got everithing! Ready to go 🌴'
+          : `You have ${numItems} items on your list and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
