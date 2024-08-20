996daad98f65808476dc040acdfa6fb2c071e2a7
kpekala <kgpekala@gmail.com>
2024-08-20 11:47:08 +0200

    display new friend form

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -1,4 +1,5 @@
 import './index.css';
+import { useState } from 'react';
 
 const initialFriends = [
   {
@@ -21,12 +22,31 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return (
+    <button className='button' onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className='app'>
       <div className='sidebar'>
         <FriendsList />
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? 'Close' : 'Add friend'}
+        </Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -59,7 +79,43 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className='button'>Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function FormAddFriend() {
+  return (
+    <form className='form-add-friend'>
+      <label>🙋Friend name</label>
+      <input type='text' />
+
+      <label>🖼️Image URL</label>
+      <input type='text' />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className='form-split-bill'>
+      <h2>Split a bill with Adam</h2>
+      <label>💰Bill value</label>
+      <input type='text' />
+
+      <label>🙋Your expense</label>
+      <input type='text' />
+
+      <label>🙋Adam's expense</label>
+      <input type='text' disabled />
+
+      <label>🤑Who is paying the bill?</label>
+      <select>
+        <option value='user'>You</option>
+        <option value='friend'>Adam</option>
+      </select>
+      <Button>Split bill</Button>
+    </form>
   );
 }
