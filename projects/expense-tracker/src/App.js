a776b89a3157b06b18658dd5ae5a7d9cb87ef4f1
kpekala <kgpekala@gmail.com>
2024-08-21 12:27:45 +0200

    finish main functionality

--- projects/expense-tracker/src/App.js
+++ projects/expense-tracker/src/App.js
@@ -1,83 +1,156 @@
 import { useState } from 'react';
 import './styles.css';
+import { FormAddLabel } from './FormAddLabel';
+
+function initData() {
+  let mockLabels = ['🍿Streaming', '📈Investments', '🪙Savings'];
+  let mockTransactions = [];
+  let initialLabels = localStorage.getItem('labels')?.split(',') ?? mockLabels;
+  let initialTransactions = JSON.parse(
+    localStorage.getItem('transactions') ?? mockTransactions
+  );
+  return [initialLabels, initialTransactions];
+}
 
 export default function App() {
-  const [transactions, setTransactions] = useState([
-    {
-      title: 'Netflix',
-      label: '🍿Streaming',
-      price: -18,
-      currency: 'PLN',
-      id: 1,
-    },
-  ]);
-  const [showTransactionForm, setShowTransactionForm] = useState(false);
-
-  function handleShowTransactionForm() {
-    setShowTransactionForm((show) => !show);
-  }
+  const [transactions, setTransactions] = useState([]);
+  const [labels, setLabels] = useState([]);
 
   function onAddTransaction(transaction) {
-    setTransactions((transactions) => [...transactions, {...transaction, id: transactions.length + 1, currency: 'PLN'}]);
-    setShowTransactionForm(false);
+    setTransactions((transactions) => [
+      ...transactions,
+      { ...transaction, id: transactions.length + 1, currency: 'PLN' },
+    ]);
+  }
+
+  function handleExport() {
+    localStorage.setItem('labels', labels);
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+  }
+
+  function handleImport() {
+    let [initialLabels, savedTransactions] = initData();
+    setLabels(initialLabels);
+    setTransactions(savedTransactions);
+  }
+
+  function handleClear() {
+    localStorage.clear();
+    setLabels([]);
+    setTransactions([]);
+  }
+
+  function onAddLabel(label) {
+    setLabels((labels) => [...labels, label]);
   }
 
   const expenses = transactions
-    .filter((transaction) => transaction.price < 0)
+    .filter((transaction) => transaction.type === 'expense')
     .reduce((partialSum, a) => partialSum - a.price, 0);
 
   const income = transactions
-    .filter((transaction) => transaction.price > 0)
+    .filter((transaction) => transaction.type === 'income')
+    .reduce((partialSum, a) => partialSum + a.price, 0);
+
+  const investment = transactions
+    .filter((transaction) => transaction.type === 'investment')
+    .reduce((partialSum, a) => partialSum + a.price, 0);
+
+  const savings = transactions
+    .filter((transaction) => transaction.type === 'savings')
     .reduce((partialSum, a) => partialSum + a.price, 0);
 
   return (
     <div className='app-container'>
       <h2 className='summary'>Summary</h2>
+      <span className='export-container'>
+        <Button onClick={handleExport}>Save</Button>
+        <Button onClick={handleImport}>Load</Button>
+        <Button onClick={handleClear}>Clear</Button>
+      </span>
       <MoneySummary money={income - expenses} text='Net Total' currency='PLN' />
       <SummaryList
-        income={income}
-        expenses={expenses}
-        investment={100}
-        savings={50}
+        income={Number(income)}
+        expenses={Number(expenses)}
+        investment={investment}
+        savings={savings}
         currency={'PLN'}
       />
       <TransactionsHeader
-        onShowTransactionForm={handleShowTransactionForm}
-        showTransactionForm={showTransactionForm}
         onAddTransaction={onAddTransaction}
+        labels={labels}
+        onAddLabel={onAddLabel}
       />
       <Transactions transactions={transactions} />
     </div>
   );
 }
 
-function TransactionsHeader({onShowTransactionForm, showTransactionForm, onAddTransaction}) {
+function TransactionsHeader({ onAddTransaction, labels, onAddLabel }) {
+  const [showTransactionForm, setShowTransactionForm] = useState(false);
+  const [showLabelForm, setShowLabelForm] = useState(false);
+
+  function handleToggleTransactionForm(show) {
+    setShowTransactionForm(show);
+    if (show) setShowLabelForm(false);
+  }
+
+  function handleToggleLabelForm(show) {
+    setShowLabelForm(show);
+    if (show) setShowTransactionForm(false);
+  }
+
+  function handleAddPanel(label) {
+    onAddLabel(label);
+    setShowLabelForm(false);
+  }
+
+  function handleAddTransaction(transaction) {
+    onAddTransaction(transaction);
+    setShowTransactionForm(false);
+  }
+
   return (
     <div>
       <div className='transactions-header'>
         <p className='transactions-title'>Transactions</p>
         <div className='buttons-group'>
-          <Button onClick={() => onShowTransactionForm()}>
+          <Button
+            onClick={() => handleToggleTransactionForm(!showTransactionForm)}
+          >
             {!showTransactionForm ? '+ Add transaction' : 'Close'}
           </Button>
-          <Button>+ Add label</Button>
+          <Button onClick={() => handleToggleLabelForm(!showLabelForm)}>
+            {!showLabelForm ? '+ Add label' : 'Close'}
+          </Button>
         </div>
       </div>
       {showTransactionForm && (
-        <FormAddTransaction onAddTransaction={onAddTransaction} />
+        <FormAddTransaction
+          onAddTransaction={handleAddTransaction}
+          labels={labels}
+        />
       )}
+      {showLabelForm && <FormAddLabel onAddLabel={handleAddPanel} />}
     </div>
   );
 }
 
-function FormAddTransaction({onAddTransaction}) {
+function FormAddTransaction({ onAddTransaction, labels }) {
+  console.log(labels);
   const [title, setTitle] = useState('');
-  const [label, setLabel] = useState('');
+  const [label, setLabel] = useState(labels ? labels[0] : '');
   const [price, setPrice] = useState(0);
+  const [type, setType] = useState('expense');
 
   function handleAddTransaction(e) {
     e.preventDefault();
-    onAddTransaction({ title, label, price });
+    onAddTransaction({
+      title,
+      label,
+      price: type === 'expense' ? -price : price,
+      type,
+    });
   }
 
   return (
@@ -94,12 +167,17 @@ function FormAddTransaction({onAddTransaction}) {
         </span>
         <span className='input-container'>
           <label>Label: </label>
-          <input
+          <select
             className='input'
-            type='text'
             value={label}
             onChange={(e) => setLabel(e.target.value)}
-          />
+          >
+            {labels.map((label, index) => (
+              <option value={label} key={index}>
+                {label}
+              </option>
+            ))}
+          </select>
         </span>
         <span className='input-container'>
           <label>Price: </label>
@@ -107,8 +185,21 @@ function FormAddTransaction({onAddTransaction}) {
             className='input'
             type='text'
             value={price}
-            onChange={(e) => setPrice(e.target.value)}
+            onChange={(e) => setPrice(Number(e.target.value))}
           />
+        </span>
+        <span className='input-container'>
+          <label>Type: </label>
+          <select
+            className='input'
+            value={type}
+            onChange={(e) => setType(e.target.value)}
+          >
+            <option value='income'>Income</option>
+            <option value='expense'>Expense</option>
+            <option value='investment'>Investment</option>
+            <option value='savings'>Savings</option>
+          </select>
         </span>
         <Button onClick={handleAddTransaction}>Add</Button>
       </div>
@@ -183,8 +274,7 @@ function Transaction({ transaction }) {
     </li>
   );
 }
-
-function Button({ children, onClick }) {
+export function Button({ children, onClick }) {
   return (
     <button className='button' onClick={onClick}>
       {children}
---
+++ projects/expense-tracker/src/FormAddLabel.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import '../src/styles.css';
+import { Button } from './App';
+
+export function FormAddLabel({ onAddLabel }) {
+  const [label, setLabel] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAddLabel(label);
+  }
+  return (
+    <form className='form-add-label' onSubmit={handleSubmit}>
+      <div className='add-label-container'>
+        <span className='input-container'>
+          <label>Label: </label>
+          <input
+            className='input'
+            value={label}
+            onChange={(e) => setLabel(e.target.value)}
+          />
+        </span>
+        <Button type='submit'>+ Add label</Button>
+      </div>
+    </form>
+  );
+}
