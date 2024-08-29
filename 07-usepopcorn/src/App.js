f0307a9c7770380d62a5a9e7a73ca54519310de1
kpekala <kgpekala@gmail.com>
2024-08-29 11:55:07 +0200

    add loader

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -56,16 +56,19 @@ const average = (arr) =>
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = 'interstellar';
 
   useEffect(() => {
     const fetchMovies = async () => {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apiKey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
       console.log(data.Search);
+      setIsLoading(false);
     };
     fetchMovies();
   }, []);
@@ -77,9 +80,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <>
@@ -90,6 +91,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className='loadre'>Loading...</p>;
 }
 
 function NavBar({ children }) {
