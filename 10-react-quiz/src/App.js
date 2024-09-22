a43c3f3bca668cb40cfe96f818f47f5a85f46333
Konrad Pękala <kgpekala@gmail.com>
2024-09-22 11:19:58 +0200

    init react quiz app

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,8 +1,17 @@
-import DateCounter from './DateCounter.js';
 import './index.css';
+import Main from './Main';
+import Header from './Header';
 
 function App() {
-  return <DateCounter />;
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
