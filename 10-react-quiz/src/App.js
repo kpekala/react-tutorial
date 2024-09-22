e62b1f74f7cbc82f78fa2b12cb715a2c70ddffe3
Konrad Pękala <kgpekala@gmail.com>
2024-09-22 15:35:37 +0200

    fake api

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,8 +1,40 @@
 import './index.css';
 import Main from './Main';
 import Header from './Header';
+import { useEffect, useReducer } from 'react';
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Action unkonwn');
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
   return (
     <div className="app">
       <Header />
