6c1846b2d4997e360da163de3918b6dce8a1c9ae
Konrad Pękala <kgpekala@gmail.com>
2024-09-23 20:18:45 +0200

    finish quiz

--- 10-react-quiz/src/components/NextButton.js
+++ 10-react-quiz/src/components/NextButton.js
@@ -1,14 +1,26 @@
-function NextButton({ dispatch, answer }) {
-  if (!answer) return null;
+function NextButton({ dispatch, answer, index, numQuestions }) {
+  console.log(index, numQuestions);
+  if (answer === null) return null;
 
-  return (
-    <button
-      className="btn btn-ui"
-      onClick={() => dispatch({ type: 'newQuestion' })}
-    >
-      Next
-    </button>
-  );
+  if (index < numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'newQuestion' })}
+      >
+        Next
+      </button>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'finish' })}
+      >
+        Finish
+      </button>
+    );
 }
 
 export default NextButton;
