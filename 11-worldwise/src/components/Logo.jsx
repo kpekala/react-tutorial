49150b7185e648d2dda78ad16877e0a89bcb3630
Konrad Pękala <kgpekala@gmail.com>
2024-10-01 08:38:19 +0200

    build app layout

---
+++ 11-worldwise/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import styles from './Logo.module.css';
+
+function Logo() {
+  return (
+    <img
+      src="src/assets/logo.png"
+      alt="WorldWise logo"
+      className={styles.logo}
+    />
+  );
+}
+
+export default Logo;
