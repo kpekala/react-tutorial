4fdde88f9a2094d741a9a6f66a3056ead3c346c2
Konrad Pękala <kgpekala@gmail.com>
2024-10-01 08:56:21 +0200

    nester routes

--- 11-worldwise/src/components/Logo.jsx
+++ 11-worldwise/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.css';
 function Logo() {
   return (
     <img
-      src="src/assets/logo.png"
+      src="../src/assets/logo.png"
       alt="WorldWise logo"
       className={styles.logo}
     />
--- 11-worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/src/components/Sidebar.jsx
@@ -4,14 +4,14 @@ import AppNav from './AppNav';
 import styles from './Sidebar.module.css';
 import Footer from './Footer';
 
+import { Outlet } from 'react-router-dom';
+
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-
-      <p>List of cities</p>
-
+      <Outlet />
       <Footer />
     </div>
   );
